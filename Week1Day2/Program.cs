68b658c26e7092facabb184e64249b892022416f
mahajanl <lakshamima@gmail.com>
2017-09-26 14:19:29 -0400

    Practice with Data Types

--- Week1Day2/Program.cs
+++ Week1Day2/Program.cs
@@ -10,6 +10,23 @@ namespace Week1Day2
     {
         static void Main(string[] args)
         {
+            //What data types have we talked about so far int and string 
+            //An int is a whole number and a stirng is a collection of characters
+
+            char firstInitial = 'L';
+            //Characters or chars are single letters or symbols
+
+            bool isTired = true;
+            //Boolean or bool is a true or false 
+
+            //other number data types
+            float number = 2.164403304907294846437820128746f;
+            decimal partialNum = 2.164403304907294846437820128746m;
+            double someNumber = 2.164403304907294846437820128746d;
+
+            Console.WriteLine(number);
+            Console.WriteLine(partialNum);
+            Console.WriteLine(someNumber);
         }
     }
 }
